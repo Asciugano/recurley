5b120ab32f968a10d5eaec2b9926c4dfdd34fc05
Asciugano <diaferiosamuele@gmail.com>
2026-08-23 16:20:06 +0200

    iniziato a lavorare con expo e nativewind

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 import "@/global.css";
 
-export default function RootLayout() {
+export default function Layout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
